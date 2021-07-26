fb9864ae6903f495c9146c057a11cbf8565365d0
zlatnaspirala <zlatnaspirala@gmail.com>
2021-07-27 00:07:47 +0200

    [format]

--- App.js
+++ App.js
@@ -19,7 +19,6 @@ if ('serviceWorker' in navigator) {
   console.warn('Matrix Engine: No support for web workers in this browser.');
 }
 
-
 window.webGLStart = () => {
   world = matrixEngine.matrixWorld.defineworld(canvas);
   world.callReDraw();
@@ -29,55 +28,52 @@ window.webGLStart = () => {
   window.App = App;
   window.runThis = runThis;
 
-  matrixEngine.utility.E("debugBox").style.display = "block"
-canvas.addEventListener('mousedown', (ev) => {
-  const { clientX, clientY, screenX, screenY } = ev;
-  
-  var outp = mat4.create();
-  var outv = mat4.create();
-  const ray = raycaster.unproject(
-      [clientX, clientY],
-      [ 0, 0, screenX, screenY ], //assume the canvas is full-size
-       mat4.invert(outp, world.pMatrix), //your invert projection matrix
-      mat4.invert(outv, App.scene.MyColoredTriangle1.mvMatrix), //your invert view matrix
-  );
+  matrixEngine.utility.E('debugBox').style.display = 'block';
+  canvas.addEventListener('mousedown', (ev) => {
+    const {clientX, clientY, screenX, screenY} = ev;
 
-  const intersectionPoint = vec3.create();
-/* const triangle = [ //example triangle
+    var outp = mat4.create();
+    var outv = mat4.create();
+    const ray = raycaster.unproject(
+      [clientX, clientY],
+      [0, 0,  ev.target.width, ev.target.height ], // assume the canvas is full-size
+      // world.pMatrix, // your invert projection matrix
+      // App.scene.MyColoredTriangle1.mvMatrix // your invert view matrix
+      mat4.invert(outp, world.pMatrix), // your invert projection matrix
+      mat4.invert(outv, App.scene.MyColoredTriangle1.mvMatrix) //your invert view matrix
+    );
+
+    const intersectionPoint = vec3.create();
+    /* const triangle = [ //example triangle
       vec3.fromValues(-1, -1, 0),
       vec3.fromValues(1, 1, 0),
       vec3.fromValues(-1, 1, 0),
   ]; */
 
- 
-  const triangle = [
-    [App.scene.MyColoredTriangle1.geometry.vertices[0],
-    App.scene.MyColoredTriangle1.geometry.vertices[1],
-    App.scene.MyColoredTriangle1.geometry.vertices[2] ],
-    [ App.scene.MyColoredTriangle1.geometry.vertices[3],
-    App.scene.MyColoredTriangle1.geometry.vertices[4],
-    App.scene.MyColoredTriangle1.geometry.vertices[5] ],
-    [App.scene.MyColoredTriangle1.geometry.vertices[6],
-    App.scene.MyColoredTriangle1.geometry.vertices[7],
-    App.scene.MyColoredTriangle1.geometry.vertices[8] ]]; 
+    const triangle = [
+      [App.scene.MyColoredTriangle1.geometry.vertices[0], App.scene.MyColoredTriangle1.geometry.vertices[1], App.scene.MyColoredTriangle1.geometry.vertices[2]],
+      [App.scene.MyColoredTriangle1.geometry.vertices[3], App.scene.MyColoredTriangle1.geometry.vertices[4], App.scene.MyColoredTriangle1.geometry.vertices[5]],
+      [App.scene.MyColoredTriangle1.geometry.vertices[6], App.scene.MyColoredTriangle1.geometry.vertices[7], App.scene.MyColoredTriangle1.geometry.vertices[8]]
+    ];
 
-
-  if (raycaster.rayIntersectsTriangle(
+    if (
+      raycaster.rayIntersectsTriangle(
         vec3.fromValues(
-          0, // matrixEngine.Events.camera.xPos,
-          0, // matrixEngine.Events.camera.yPos,
-          0)
-      , // your camera position - rayOrigin
-      ray,
-      triangle,
-      intersectionPoint,
-  )) {
+          matrixEngine.Events.camera.xPos,
+          matrixEngine.Events.camera.yPos,
+          matrixEngine.Events.camera.zPos
+        ), // your camera position - rayOrigin
+        ray,
+        triangle,
+        intersectionPoint
+      )
+    ) {
       console.log('hits', intersectionPoint);
-      matrixEngine.utility.E("debugBox").style.background = "red"
-  } else {
-    matrixEngine.utility.E("debugBox").style.background = "green"
-  }
-})
+      matrixEngine.utility.E('debugBox').style.background = 'red';
+    } else {
+      matrixEngine.utility.E('debugBox').style.background = 'green';
+    }
+  });
 
   // If you need this , you can prolong loading time
   setTimeout(() => {
@@ -86,7 +82,7 @@ canvas.addEventListener('mousedown', (ev) => {
 
   // Run example
   // runThis(world);
-}
+};
 
 window.matrixEngine = matrixEngine;
 var App = matrixEngine.App;
