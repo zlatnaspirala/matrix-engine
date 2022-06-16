408dc82904b75a13747abef1baffcc1f6ad9f210
zlatnaspirala <zlatnaspirala@gmail.com>
2022-06-16 21:20:18 +0200

    Update bvh-loader.js

--- apps/bvh-loader.js
+++ apps/bvh-loader.js
@@ -1,8 +1,9 @@
 
 // prodc
-import MEBvh from 'bvh-loader';
-// import MEBvh from '../node_modules/bvh-loader/index';
+// import MEBvh from 'bvh-loader';
+import MEBvh from '../node_modules/bvh-loader/index';
 import App from '../program/manifest';
+// import * as matrixEngine from '../index.js';
 
 /**
  * @description
