7c2739467326e57d5b71f8053c56ca69b6fa2ff4
zlatnaspirala <zlatnaspirala@gmail.com>
2022-09-04 13:49:23 +0200

    wip

--- apps/adding_color_cube.js
+++ apps/adding_color_cube.js
@@ -9,13 +9,14 @@ import App from "../program/manifest";
 
 export var runThis = (world) => {
 
+  App.camera.SceneController = true;
+
   var textuteImageSamplers = {
     source: ["res/images/complex_texture_1/diffuse.png"],
     mix_operation: "multiply",
   };
 
-
-  world.Add("cube", 1, "MyColoredCube1");
+  // world.Add("cube", 1, "MyColoredCube1");
 
   world.Add("cubeLightTex", 1, "MyColoredCube2", textuteImageSamplers);
 
@@ -27,14 +28,23 @@ export var runThis = (world) => {
   // addEventListener("ray.hit.event", function(e) {
   //   console.info(e.detail);
   // });
-    App.scene.MyColoredCube1.position.SetX(-3);
-    App.scene.MyColoredCube1.position.SetZ(-11);
-    App.scene.MyColoredCube1.rotation.rotx = 45;
+    // App.scene.MyColoredCube1.position.SetX(-3);
+    // App.scene.MyColoredCube1.position.SetZ(-11);
+    // App.scene.MyColoredCube1.rotation.rotx = 45;
 
-    App.scene.MyColoredCube3.position.SetX(2);
+    App.scene.MyColoredCube3.position.SetZ(-11);
+    App.scene.MyColoredCube3.position.SetX(0);
+    App.scene.MyColoredCube3.position.SetY(-2);
+    App.scene.MyColoredCube3.geometry.setScale(4)
+    App.scene.MyColoredCube3.rotation.rotx = 90;
+    App.scene.MyColoredCube3.shadows.innerLimit = 0
+    
+    App.scene.MyColoredCube2.shadows.lightPosition= [0,1,3]
 
+    App.scene.MyColoredCube2.shadows.innerLimit = 0
     App.scene.MyColoredCube2.position.SetX(0);
     App.scene.MyColoredCube2.position.SetZ(-11);
-    App.scene.MyColoredCube2.rotation.rotx = 45;
+    App.scene.MyColoredCube3.position.SetY(-1);
+    // App.scene.MyColoredCube2.rotation.rotx = 45;
 
 };
