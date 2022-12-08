edac46c5333f1ce6af97bf0374e7a2ce77e755eb
zlatnaspirala <zlatnaspirala@gmail.com>
2022-12-08 20:19:01 +0100

    Update a2.js

--- 2DTextureEditor/cache/pilGreen/a2.js
+++ 2DTextureEditor/cache/pilGreen/a2.js
@@ -1,1 +1,1 @@
-  actualTexture.ENGINE.MODULES.ACCESS_MODULE( 'STARTER').NEW_OBJECT('pilGreen',8 , 37,90,10); 
+  actualTexture.ENGINE.MODULES.ACCESS_MODULE( 'STARTER').NEW_OBJECT('pilGreen',8 , 44,90,10); 
