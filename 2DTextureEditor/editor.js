17adc1f9003479ac9cf58b656cd5b58ec7416ca1
zlatnaspirala <zlatnaspirala@gmail.com>
2023-12-08 16:23:36 +0100

    Added in install.dep  node install_paths.js

--- 2DTextureEditor/editor.js
+++ 2DTextureEditor/editor.js
@@ -3,8 +3,8 @@ let tools = require('visual-js-server');
 
 let config = {
   VERSION : "3.0.0",
-  PATH_OF_NODE_APP : "<PATH_OF_NODE_APP>",
-  PATH_OF_WWW : "<PATH_OF_WWW>",
+  PATH_OF_NODE_APP : "G:/web_server/xampp/htdocs/PRIVATE_SERVER/me/me/2DTextureEditor/",
+  PATH_OF_WWW : "G:/web_server/xampp/htdocs/PRIVATE_SERVER/me/me/2DTextureEditor/",
   EDITOR_PORT : "1013",
   DESTROY_SESSION_AFTER_X_mSECUNDS : 20000,
   SELF_HOST: {
