f415fdb781ee2d431f6659b568542bba81adc830
zlatnaspirala <zlatnaspirala@gmail.com>
2023-12-08 16:12:18 +0100

    wip

--- multiplatform/win/cef-sharp/MatrixEngineGUI.cs
+++ multiplatform/win/cef-sharp/MatrixEngineGUI.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,17 +18,27 @@ namespace matrix_engine
         {
             InitializeComponent();
             string URLStart = "https://fps-matrix-engine.vercel.app";
-
-            if (args.ToString() != "")
-            {
+            var APP_PREDEFINED_FILE = System.Reflection.Assembly.GetEntryAssembly().Location + @"\me.txt";
+            APP_PREDEFINED_FILE = APP_PREDEFINED_FILE.ToString().Replace(@"matrix-engine.exe\", @"");
+            // MessageBox.Show(APP_PREDEFINED_FILE, File.Exists(APP_PREDEFINED_FILE).ToString());
+            if (args.ToString() != "") {
+                //  First priory is args ho comes with ./matrix-engine.exe url=localhost
                 URLStart = args.Replace("url=", "");
                 chromiumWebBrowser1.LoadUrl(URLStart);
-            }
-            else {
+            } else if (File.Exists(APP_PREDEFINED_FILE)) {
+                // Next is me.txt
+                StreamReader sr = new StreamReader(APP_PREDEFINED_FILE);
+                var objects = JToken.Parse(sr.ReadToEnd().ToString());
+                // MessageBox.Show("APP_STATUS: \n " + objects.Root[objects.Root["APP_STATUS"].ToString()], objects.Root["APP_STATUS"].ToString());
+                // APP_STATUS
+                URLStart = objects.Root[objects.Root["APP_STATUS"].ToString()].ToString();
+                // 
+                chromiumWebBrowser1.LoadUrl(URLStart);
+
+            } else {
                 chromiumWebBrowser1.LoadUrl(URLStart);
                 this.Text = "Matrix-Engine [" + URLStart + "]";
             }
-
         }
 
         private void chromiumWebBrowser1_LoadingStateChanged(object sender, CefSharp.LoadingStateChangedEventArgs e)
