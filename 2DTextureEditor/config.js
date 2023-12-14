4d8d63b876229663df46948fb2b3398f40412112
zlatnaspirala <zlatnaspirala@gmail.com>
2023-12-14 23:36:25 +0100

    fix

--- 2DTextureEditor/config.js
+++ 2DTextureEditor/config.js
@@ -1,8 +1,8 @@
 
 module.exports = {
   VERSION : "3.0.0",
-  PATH_OF_NODE_APP : "G:/web_server/xampp/htdocs/PRIVATE_SERVER/me/me/2DTextureEditor/",
-  PATH_OF_WWW : "G:/web_server/xampp/htdocs/PRIVATE_SERVER/me/me/2DTextureEditor/",
+  PATH_OF_NODE_APP : "<PATH_OF_NODE_APP>",
+  PATH_OF_WWW : "<PATH_OF_WWW>",
   EDITOR_PORT : "1013",
   DESTROY_SESSION_AFTER_X_mSECUNDS : 20000,
   SELF_HOST: {
