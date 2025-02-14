b6f79894c8db50d7051b2dea5f5dc243fccb873f
zlatnaspirala <zlatnaspirala@gmail.com>
2025-02-14 16:36:22 +0100

    Update AssemblyInfo.cs

--- multiplatform/win/cef-sharp/Properties/AssemblyInfo.cs
+++ multiplatform/win/cef-sharp/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("matrix-engine")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("matrix-engine is open source webgl project. New networking driver for kurentu OpenVidu server middleware.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nikola Lukic")]
 [assembly: AssemblyProduct("matrix-engine")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("maximumroulette.com")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
