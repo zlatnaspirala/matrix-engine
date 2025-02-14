24634cb2546613f65f80669526ee8dcb58c24a3b
zlatnaspirala <zlatnaspirala@gmail.com>
2025-02-14 16:33:06 +0100

    Now build native wrapper only with msbuild.exe no need visual studio

--- desktop-build.bat
+++ desktop-build.bat
@@ -1,10 +1,39 @@
-REM set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v2.0.50727
-REM set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v3.5
-REM set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
-REM set msBuildDir=C:\Program Files (x86)\MSBuild\12.0\Bin
-set msBuildDir=C:\Program Files (x86)\MSBuild\14.0\Bin
+REM --------------------------------------------------------
+REM Setup script for matrix-engine native builds for windows
+REM --------------------------------------------------------
 
+REM dotnet nuget locals all --list
+
+@echo off
+@REM setlocal enabledelayedexpansion
+
+rem List of possible paths for msbuild.exe
+set paths=("C:\Program Files (x86)\MSBuild\12.0\Bin" "C:\Program Files (x86)\MSBuild\14.0\Bin" "C:\Program Files\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin" "C:\Program Files\Microsoft Visual Studio\2022\Preview\MSBuild\Current\Bin" "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin" "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin" "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin" "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin" "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin" "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin" "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin" "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin" "C:\Windows\Microsoft.NET\Framework\v4.0.30319" "C:\Windows\Microsoft.NET\Framework\v4.0.30319" "C:\Windows\Microsoft.NET\Framework\v3.5" "C:\Windows\Microsoft.NET\Framework\v2.0.50727")
+
+@rem Check each path for msbuild.exe
+for %%p in %paths% do (
+  if exist %%~dpnxp (
+    echo msbuild.exe found in: %%~p
+    set msBuildDir=%%~p
+    set msbuild_path=%%~p
+    goto :found
+  )
+)
+
+:found
+@REM endlocal 
+
+@REM set msBuildDir=C:\Program Files (x86)\MSBuild\14.0\Bin
 SET mypath=%~dp0
 echo %mypath:~0,-1%
+set PROJECT=%mypath%multiplatform\win\cef-sharp
+echo "Check packages..."
+call cd %PROJECT%
+echo Download latest Nuget.exe
+REM Standard win tool bitsadmin.exe https://www.nuget.org/downloads
+call bitsadmin.exe /transfer "GetNuget" https://dist.nuget.org/win-x86-commandline/latest/nuget.exe %PROJECT%\nuget.exe
+echo "nuget restore, please wait..."
+call nuget restore
+call "%msBuildDir%\msbuild.exe" "%mypath%\multiplatform\win\cef-sharp\matrix-engine.sln" /p:Configuration=Release
 
-call "%msBuildDir%\msbuild.exe" "%mypath%\multiplatform\win\cef-sharp\matrix-engine.sln" /p:Configuration=Release /l:FileLogger,Microsoft.Build.Engine;logfile=Manual_MSBuild_ReleaseVersion_LOG.log
+PAUSE
